9c43ed995ae841b6c71f53ae027303b3a921e3b3
Swarnavo2003 <swarnavo2020@gmail.com>
2025-10-23 21:59:22 +0530

    moneymanager backend first commit

---
+++ src/main/java/in/swarnavo/moneymanager/MoneymanagerApplication.java
@@ -0,0 +1,11 @@
+package in.swarnavo.moneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(MoneymanagerApplication.class, args);
+	}
+}
